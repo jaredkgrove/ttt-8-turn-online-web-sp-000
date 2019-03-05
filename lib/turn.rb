0bb59af4e133a6a579f8e2266b65ab473a0d22e2
Jared Grove <jaredkgrove@gmail.com>
2019-03-05 04:26:39 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,44 @@
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.chomp
+  index = input_to_index(input)
+  until valid_move?(index)
+  
+ 
+  if valid_move?(index)
+    move(board, index)
+  end
+  
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def move(board, index, mark = "X")
+  board[index] == mark
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
+    TRUE
+  else
+    FALSE
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == NIL
+    FALSE
+  else
+    TRUE
+  end
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
